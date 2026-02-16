ac8530b660ed1b7cdd1ebe01b7478042bc5effac
4run-Rangad <arunrangad83@gmail.com>
2026-02-16 15:00:00 +0530

    changed the value of password variable for better understanding

--- src/util/DBConnection.java
+++ src/util/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
     public static Connection getConnection() throws SQLException {
         String url = "jdbc:mysql://localhost:3306/student_db";
         String user = "root";
-        String password = "Microm@xq413";
+        String password = "Your_Password";
 
         return DriverManager.getConnection(url,user,password);
     }
